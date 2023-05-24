23a64a73e6ac7de16cbed69d257d57313055d2bb
Albert <71594935+albertusivan@users.noreply.github.com>
2023-05-24 22:33:15 +0700

    Add unboarding

--- app/src/main/java/com/example/sabi/ui/onboarding/screen/OnboardingThirdFragment.kt
+++ app/src/main/java/com/example/sabi/ui/onboarding/screen/OnboardingThirdFragment.kt
@@ -1,12 +1,16 @@
 package com.example.sabi.ui.onboarding.screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.sabi.R
 import com.example.sabi.databinding.FragmentOnboardingThirdBinding
+import com.example.sabi.ui.home.HomeActivity
 
 class OnboardingThirdFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentOnboardingThirdBinding? = null
@@ -19,7 +23,21 @@ class OnboardingThirdFragment : Fragment(), View.OnClickListener {
         return inflater.inflate(R.layout.fragment_onboarding_third, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnNext: ImageButton = view.findViewById(R.id.btn_next)
+        btnNext.setOnClickListener(this)
+    }
+
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        if (v != null) {
+            if (v.id == R.id.btn_next) {
+                toHome()
+            }
+        }
+    }
+    private fun toHome() {
+        val intent = Intent(activity, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
