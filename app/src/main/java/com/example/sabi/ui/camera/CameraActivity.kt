55f38d9abadb7f67d9dd26893ce1698435f51f41
Albert <71594935+albertusivan@users.noreply.github.com>
2023-06-09 19:17:55 +0700

    Camera object detection prototype (using default object detection tflite)

--- app/src/main/java/com/example/sabi/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/sabi/ui/camera/CameraActivity.kt
@@ -6,17 +6,19 @@ import android.content.AsyncQueryHandler
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.SurfaceTexture
+import android.graphics.*
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.FileUtils
 import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
+import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
@@ -24,15 +26,31 @@ import androidx.core.content.ContextCompat
 import com.example.sabi.R
 import com.example.sabi.databinding.ActivityCameraBinding
 import com.example.sabi.databinding.ActivityDictonaryBinding
+import com.example.sabi.ml.SsdMobilenetV11Metadata1
+
 import com.example.sabi.ui.ViewModelFactory
 import com.example.sabi.ui.home.HomeActivity
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
 
 class CameraActivity : AppCompatActivity() {
 
+    lateinit var labels:List<String>
+    val colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED
+    )
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var bitmap: Bitmap
     lateinit var cameraDevice: CameraDevice
     lateinit var handler: Handler
     lateinit var cameManager: CameraManager
     lateinit var textureView: TextureView
+    lateinit var imageView: ImageView
+    lateinit var model: SsdMobilenetV11Metadata1
     private lateinit var binding: ActivityCameraBinding
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -42,11 +60,12 @@ class CameraActivity : AppCompatActivity() {
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        labels = FileUtil.loadLabels(this,"labels.txt")
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300,300,ResizeOp.ResizeMethod.BILINEAR)).build()
+        model = SsdMobilenetV11Metadata1.newInstance(this)
         val handlerThread = HandlerThread("videoThread")
         handlerThread.start()
         handler = Handler(handlerThread.looper)
-
-        cameManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         binding.apply {
             btnBack.setOnClickListener{
@@ -55,7 +74,9 @@ class CameraActivity : AppCompatActivity() {
             }
         }
 
+        imageView = findViewById(R.id.iv_response)
         textureView = findViewById(R.id.textureView)
+
         textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
             override fun onSurfaceTextureAvailable(
                 surface: SurfaceTexture,
@@ -78,12 +99,51 @@ class CameraActivity : AppCompatActivity() {
             }
 
             override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
 
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+
+                val outputs = model.process(image)
+                val locations = outputs.locationsAsTensorBuffer.floatArray
+                val classes = outputs.classesAsTensorBuffer.floatArray
+                val scores = outputs.scoresAsTensorBuffer.floatArray
+                val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
+
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                var canvas = Canvas(mutable)
+
+                val h = mutable.height
+                val w = mutable.width
+
+                paint.textSize= h/15f
+                paint.strokeWidth = h/85f
+
+                var x = 0
+                scores.forEachIndexed { index, fl ->
+                    x = index
+                    x *= 4
+                    if(fl > 0.5){
+                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(RectF(locations.get(x+1)*w,locations.get(x+1)*h,locations.get(x+3)*w,locations.get(x+3)*h),paint)
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(labels.get(classes.get(index).toInt())+" "+fl.toString(), locations.get(x+1)*w, locations.get(x)*h,paint)
+                    }
+                }
+
+                imageView.setImageBitmap(mutable)
             }
 
         }
 
+        cameManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
     }
 
     fun openCamera(){
